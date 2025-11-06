edf66c8de3ac24c5adc54d3923d0aea41d60e85e
ABENOJQ <quentina344@gmail.com>
2025-11-06 09:36:10 +0100

    feat(): update url to access at api meteo

--- src/Infrastructure/api_client.py
+++ src/Infrastructure/api_client.py
@@ -5,14 +5,16 @@ from src.config.Settings import Settings
 
 class APIClient:
     def __init__(
-        self, base_url: str = Settings.API_BASE_URL, timeout: int = Settings.API_TIMEOUT
+        self, base_url: str = Settings.API_BASE_URL, records_url: str = Settings.API_RECORDS_URL, timeout: int = Settings.API_TIMEOUT
     ):
         self.base_url = base_url.rstrip("/")
+        self.records_url = records_url.rstrip("/")
         self.timeout = timeout
         self.session = requests.Session()
 
     def fetch_records(self, limit: int):
-        url = f"{self.base_url}/records?limit={limit}"
+        url = f"{self.records_url}&limit={limit}"
+        
         response = self.session.get(url, timeout=self.timeout)
         response.raise_for_status()
         return response.json()
--- src/config/Settings.py
+++ src/config/Settings.py
@@ -7,6 +7,7 @@ load_dotenv()
 class Settings:
     API_BASE_URL: str = os.getenv("API_BASE_URL", "")
     API_TIMEOUT: int = int(os.getenv("API_TIMEOUT", "10"))
-
+    API_RECORDS_URL: str = os.getenv("API_RECORDS_URL", "")
+    
     if not API_BASE_URL:
         raise ValueError("La variable d'environnement API_BASE_URL est manquante")
