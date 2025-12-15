6c8ab30c3bbc0da070880c76b31d83431b16378a
ABENOJQ <quentina344@gmail.com>
2025-12-15 10:31:18 +0100

    feat(builder): update city builder with design pattern

--- __main__.py
+++ __main__.py
@@ -1,39 +1,52 @@
 import sys
 import os
+
 ROOT_DIR = os.path.dirname(__file__)  # ...\Meteo
 sys.path.append(ROOT_DIR)
 
 
 from src.Application.builder.CityBuilder import CityBuilder
 
-from src.Infrastructure.provider.CityStationConfigMemoryProvider import CityStationConfigMemoryProvider
+from src.Infrastructure.provider.CityStationConfigMemoryProvider import (
+    CityStationConfigMemoryProvider,
+)
+
 
 def main() -> int:
     city_names = ["toulouse"]
-    city_station = ["compans cafarelli", "universite paul sabatier",]
+    city_station = [
+        "compans cafarelli",
+        "universite paul sabatier",
+    ]
 
     provider = CityStationConfigMemoryProvider()
 
-    builder = CityBuilder(
-        names_city=city_names,
-        stations_choose=city_station,
-        city_station_provider=provider,
-    )
+    # builder = CityBuilder(
+    #     names_city=city_names,
+    #     stations_choose=city_station,
+    #     city_station_provider=provider,
+    # )
 
-    cities = builder.build()
+    cities_builder = (
+        CityBuilder()
+        .set_names_city(city_names)
+        .set_stations_choose(city_station)
+        .set_city_station_provider(provider)
+        .build()
+    )
 
     # print(cities["toulouse"].stations[0].name)
     # exit(0)
 
-    for name, city in cities.items():
+    for name, city in cities_builder.items():
         print(f"=== {name.upper()} ===")
         for station in city.stations:
-            print(
-                f"- Station: {station.name} ({len(station.list_of_records)} records)")
+            print(f"- Station: {station.name} ({len(station.list_of_records)} records)")
             print("  Records:")
             for record in station.list_of_records:
                 print(
-                    f"    - Date: {record.paris_date}, Temp: {record.temperature}°C, Humidity: {record.humidity}%, Pression: {record.pressure}Pa")
+                    f"    - Date: {record.paris_date}, Temp: {record.temperature}°C, Humidity: {record.humidity}%, Pression: {record.pressure}Pa"
+                )
 
     return 0
 
--- src/Application/builder/CityBuilder.py
+++ src/Application/builder/CityBuilder.py
@@ -10,18 +10,40 @@ from src.Domain.entity.ANodeLinkedList import ANodeLinkedList
 
 
 class CityBuilder(IBuilder):
-    def __init__(
-        self,
-        names_city: list[str],
-        stations_choose: list[str],
-        city_station_provider: ICityStationProvider,
-    ):
-        self.names_city = names_city
-        self.stations_choose = stations_choose
+
+    names_city: list[str]
+    stations_choose: list[str]
+    city_mapper: CityMapper
+    _city_station_provider: ICityStationProvider
+
+    def __init__(self) -> None:
         self.city_mapper = CityMapper()
+
+    def set_names_city(self, names_city: list[str]) -> None:
+        self.names_city = names_city
+        return self
+    
+    def set_stations_choose(self, stations_choose: list[str]) -> None:
+        self.stations_choose = stations_choose
+        return self
+    
+    
+    def set_city_station_provider(self, city_station_provider: ICityStationProvider) -> None:
         self._city_station_provider = city_station_provider
+        return self
+    
+
 
     def build(self) -> dict[str, ACity]:
+
+        if self.names_city is None:
+            raise ValueError("Names of cities not set")
+        if self.stations_choose is None:
+            raise ValueError("Stations to choose not set")
+        if self._city_station_provider is None:
+            raise ValueError("City station provider not set")
+        
+
         cities: dict[str, ACity] = {}
         # print("Building cities...")
         for name_city in self.names_city:
@@ -33,14 +55,13 @@ class CityBuilder(IBuilder):
             first_station = True
             for station_key in station_keys:
 
-                
                 if station_key in self.stations_choose:
-                    
+
                     if first_station:
 
                         linked_list_station = QueueList(ANodeQueueList(station_key))
                         # linked_list_station = LinkedList(ANodeLinkedList(station_key))
-                        
+
                         first_station = False
                     else:
 
@@ -56,7 +77,7 @@ class CityBuilder(IBuilder):
                     city_station_provider=self._city_station_provider,
                 ).build()
             )
-           
+
             actual_node = actual_node.get_next()
 
         city = self.city_mapper.to_object(name=name_city, list_of_stations=stations)
